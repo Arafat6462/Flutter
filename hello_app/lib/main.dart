fd8160486801262f56650a54addbc72813a83813
Arafat <arafat6462@gmail.com>
2023-01-22 16:03:42 +0600

    row, column

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -11,21 +11,26 @@ class MyFirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Center(
-        child: Container(
-          height: double.infinity, 
-          width: 200.0,
-          color: const Color.fromARGB(255, 192, 196, 199),
-          child: const Center(
-              child: Text(
-            "Hello there.",
-            style: TextStyle(
-                fontSize: 20,
-                color: Color.fromARGB(255, 90, 54, 48),
-                fontWeight: FontWeight.bold),
-          )),
+        body: Row(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 200.0,
+              width: 100.0,
+            ),
+            Container(
+              color: Colors.blue,
+              height: 200.0,
+              width: 100.0,
+            ),
+            Container(
+              color: Colors.purple,
+              height: 200.0,
+              width: 100.0,
+            ),
+          ],
         ),
-      )),
+      ),
     );
   }
 }
