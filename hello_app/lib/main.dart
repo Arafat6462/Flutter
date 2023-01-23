14a7fafacaaf6991f3c9f517119b6039130890ad
Arafat <arafat6462@gmail.com>
2023-01-23 12:30:14 +0600

    appbar

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -10,30 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        body: Container(
-          color: Color.fromARGB(255, 195, 226, 222),
-          child: Column(
-            children: [
-              Center(
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 50.0,
-                        backgroundColor: Colors.green,
-                        child: Icon(
-                          Icons.production_quantity_limits,
-                          size: 30.0,
-                        ),
-                      ))),
-              Center(
-                child: Card(
-                  elevation: 20.0,
-                  child: Image.network(
-                      "https://cdn.theatlantic.com/thumbor/viW9N1IQLbCrJ0HMtPRvXPXShkU=/0x131:2555x1568/976x549/media/img/mt/2017/06/shutterstock_319985324/original.jpg"),
-                ),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          leading: const Icon(Icons.search),
+          title: const Text('Title'),
+          actions: const [Icon(Icons.alarm), Icon(Icons.notification_add)],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
