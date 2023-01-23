83c8d906601e4942fc813d211b43a30fe36ad211
Arafat <arafat6462@gmail.com>
2023-01-23 12:18:01 +0600

    image, card

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -8,25 +8,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       home: Scaffold(
-        body: Center(
-          child: Container(
-            // child: ElevatedButton(
-            child: SizedBox(
-              width: 120.0,
-              height: 40.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  print("Button clicked.");
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.search),
-                    Text("Click me"),
-                  ],
+        body: Container(
+          color: Color.fromARGB(255, 195, 226, 222),
+          child: Column(
+            children: [
+              Center(
+                  child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundColor: Colors.green,
+                        child: Icon(
+                          Icons.production_quantity_limits,
+                          size: 30.0,
+                        ),
+                      ))),
+              Center(
+                child: Card(
+                  elevation: 20.0,
+                  child: Image.network(
+                      "https://cdn.theatlantic.com/thumbor/viW9N1IQLbCrJ0HMtPRvXPXShkU=/0x131:2555x1568/976x549/media/img/mt/2017/06/shutterstock_319985324/original.jpg"),
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
