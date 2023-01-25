43b902595e136f8dc3f732b644c7a739a57e3ca6
Arafat <arafat6462@gmail.com>
2023-01-25 15:31:44 +0600

    ..api

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,22 +1,25 @@
- import 'package:flutter/material.dart';
- 
- void main() => runApp(const MyApp());
- 
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       title: 'Material App',
-       home: Scaffold(
-         appBar: AppBar(
-           title: const Text('Material App Bar'),
-         ),
-         body: const Center(
-           child: Text('Hello World..!!'),
-         ),
-       ),
-     );
-   }
- }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+  String api = "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";
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World..!!'),
+        ),
+      ),
+    );
+  }
+}
