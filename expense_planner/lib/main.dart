f97cc648c9ab4760171f877d94e133fe9b729237
Arafat <arafat6462@gmail.com>
2023-01-31 15:17:27 +0600

    more style

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -66,9 +66,7 @@ class MyHomePage extends StatelessWidget {
                         horizontal: 15,
                       ),
                       decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.purple,
-                              width: 2)),
+                          border: Border.all(color: Colors.purple, width: 2)),
                       padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
@@ -79,9 +77,17 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tx.title),
-                        Text(tx.date.toString()),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ],
                     )
                   ],
