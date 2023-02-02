7c43699828b60e07fbe147479e99dd4e4874cd09
Arafat <arafat6462@gmail.com>
2023-02-02 16:49:47 +0600

    showing modal bottom sheet.

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:expense_planner/widgets/user_transactions.dart';
+import 'package:expense_planner/widgets/new_transaction.dart';
+import 'package:expense_planner/widgets/transacgion_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:intl/intl.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,14 +22,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // const MyHomePage({super.key});
+  // final titleController = TextEditingController();
+  // final amountController = TextEditingController();
 
-  // String titleInput = "";
-  // String amountInput = "";
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.99,
+      date: DateTime.now(),
+    )
+  ];
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +91,9 @@ class MyHomePage extends StatelessWidget {
         title: Text("Flutter App 2"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              startAddNewTransaction(context);
+            },
             icon: Icon(Icons.add),
           )
         ],
@@ -55,16 +113,17 @@ class MyHomePage extends StatelessWidget {
                 color: Color.fromARGB(255, 240, 224, 214),
               ),
             ),
-            UserTransactions()
+            TransactionList(_userTransactions)
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          startAddNewTransaction(context);
+        },
       ),
     );
   }
 }
-//04 -> 004
