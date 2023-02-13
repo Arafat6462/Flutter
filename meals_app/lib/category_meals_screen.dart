4ad607e9a3641a65a561131d4a21a248272716e1
Arafat <arafat6462@gmail.com>
2023-02-13 10:27:13 +0600

    navigating to new page

---
+++ meals_app/lib/category_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The recipies"),
+      ),
+      body: Center(
+        child: Text("The Recipies for the Categories"),
+      ),
+    );
+  }
+}
