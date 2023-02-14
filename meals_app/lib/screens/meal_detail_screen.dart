0385ce814cda47a2cb5cbdc4e1457a52d05a4e5e
Arafat <arafat6462@gmail.com>
2023-02-14 11:36:19 +0600

    navigating to the meal details

---
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)?.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(mealId)),
+      body: Center(
+        child: Text("The meal - ${mealId}"),
+      ),
+    );
+  }
+}
