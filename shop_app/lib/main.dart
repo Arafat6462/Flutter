58c6345c3caa226f88785b2e36c73533397a6488
Arafat <arafat6462@gmail.com>
2023-02-16 12:21:15 +0600

    start section 8 (State management)

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
