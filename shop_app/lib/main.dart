55293bdcca06fad14f2dbecb3aed96e99baa165e
Arafat <arafat6462@gmail.com>
2023-02-16 15:12:06 +0600

    product grid & item widgets

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
 
 void main() {
   runApp(MyApp()); 
@@ -14,23 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text("Let\'s build a shop"),
-      ),
-    );
-  }
-}
+ 
